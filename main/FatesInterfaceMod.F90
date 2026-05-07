50f3c12537a7358398bf4ff4f6888d45dfdb2e55
rosiealice <rosieafisher@gmail.com>
2026-05-07 12:38:17 +0200

    dding missing fates_hdim_levfuel definition

--- main/FatesInterfaceMod.F90
+++ main/FatesInterfaceMod.F90
@@ -1204,12 +1204,15 @@ contains
        integer :: iel
        integer :: ilu
 
+
+
        allocate( fates_hdim_levsclass(1:nlevsclass   ))
        allocate( fates_hdim_pfmap_levscpf(1:nlevsclass*numpft))
        allocate( fates_hdim_scmap_levscpf(1:nlevsclass*numpft))
        allocate( fates_hdim_levpft(1:numpft   ))
        allocate( fates_hdim_levlanduse(1:n_landuse_cats))
-       allocate( fates_hdim_levemis(1:num_emission_compounds   ))
+       allocate( fates_hdim_levemis(1:num_emission_compounds))
+       allocate( fates_hdim_levfuel(1:num_fuel_classes))
        allocate( fates_hdim_levcwdsc(1:NCWD   ))
        allocate( fates_hdim_levage(1:nlevage   ))
        allocate( fates_hdim_levheight(1:nlevheight   ))
@@ -1261,7 +1264,7 @@ contains
        do ipft=1,numpft
           fates_hdim_levpft(ipft) = ipft
        end do
-
+       
        ! make fuel array
        do ifuel=1,num_fuel_classes
           fates_hdim_levfuel(ifuel) = ifuel
